894eb98da6381c15e676f7e59f9352530b76be84
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-05-03 21:18:49 -0300

    Avancando no desafio 5

--- Abr24/AppsGerenciadas_CDN_AWS/desafio5/CDN_ECS_ALB/deploy.sh
+++ Abr24/AppsGerenciadas_CDN_AWS/desafio5/CDN_ECS_ALB/deploy.sh
@@ -1,2 +1,2 @@
 ./build.sh
-aws ecs update-service --cluster arn:aws:ecs:us-east-1:905418339132:cluster/biacdnalb --service bia_wordpress_service  --force-new-deployment
+aws ecs update-service --cluster cdnalb --service serv-task-def1  --force-new-deployment
