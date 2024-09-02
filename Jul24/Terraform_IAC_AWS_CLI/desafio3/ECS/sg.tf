c111e5f4144f2e6e930df28523a3486cab6b3ba0
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-09-02 17:17:09 -0300

    Creating ALB

--- Jul24/Terraform_IAC_AWS_CLI/desafio3/ECS/sg.tf
+++ Jul24/Terraform_IAC_AWS_CLI/desafio3/ECS/sg.tf
@@ -49,3 +49,46 @@ resource "aws_security_group" "bia-web" {
   tags_all               = {}
   vpc_id                 = "vpc-038a32576faa90bfb"
 }
+
+
+resource "aws_security_group" "bia-alb" {
+  description = "acesso do bia alb"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "liberado para o mundo"
+    from_port        = 443
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 443
+    }, {
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "liberado para o mundo"
+    from_port        = 80
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 80
+  }]
+  name                   = "bia-alb"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = {}
+  tags_all               = {}
+  vpc_id                 = "vpc-038a32576faa90bfb"
+}
