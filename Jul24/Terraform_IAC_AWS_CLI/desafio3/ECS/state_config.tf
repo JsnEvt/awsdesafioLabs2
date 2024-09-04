28512e9077b09d93b49ae834bdd7a1e8e3886543
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-09-04 19:12:22 -0300

    Before of destruction of the resources

--- Jul24/Terraform_IAC_AWS_CLI/desafio3/ECS/state_config.tf
+++ Jul24/Terraform_IAC_AWS_CLI/desafio3/ECS/state_config.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "local" {
-    # bucket  = "bia-s3-terraform"
-    # key     = "ecs/terraform.tfstate"
-    # region  = "us-east-1"
-    # profile = "jason"
+  backend "s3" {
+    bucket  = "bia-s3-terraform"
+    key     = "ecs/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "jason"
   }
 }
