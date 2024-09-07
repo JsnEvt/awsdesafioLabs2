c9707f1e5805bf5aba2840d4082ff56c9f7372fa
Jason Santos <110934550+JsnEvt@users.noreply.github.com>
2024-09-07 18:16:01 -0300

    Initalizing SAM/AWS Lambda

---
+++ Ago24/RDS_e_Banco_Relacional/desafio4/BastionHost_Lambda/porteiro/src/handlers/porteiro.js
@@ -0,0 +1,32 @@
+/*
+Enquanto o Terraform provisiona os recursos na AWS com o IAC(infrastructure-as-code)
+o SAM(Serverless Application Model) serve como estrutura de funcoes que podem ser
+implementadas em Lambdas na AWS para controlar/operar sobre os recursos criados no
+provedor cloud para AUTOMATIZAR PROCESSOS.
+*/
+
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "us-east-1" });
+const ec2 = new AWS.EC2({ apiVersion: "2016-11-15" });
+const params = {
+  InstanceIds: ["i-017cfa4264fba5419"],
+}
+
+export.startEC2Instances = () = {
+  return ec2.startInstances(params, function (err, data) {
+    if (err)
+      console.log(err, err.stack)
+    else
+      console.log("Porteiro ligado com sucesso")
+  })
+}
+
+export.stopEC2Instances = () = {
+  return ec2.stopInstances(params, function (err, data) {
+    if (err)
+      console.log(err, err.stack)
+    else
+      console.log("Porteiro desligado com sucesso")
+  })
+}
